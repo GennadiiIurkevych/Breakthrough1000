29924d11d2a77c1e0abd6cdbd7d89aee5d8b5ee0
Gennadii <yurgensport@gmail.com>
2022-07-25 16:27:10 +0300

    created components

---
+++ src/components/health/health.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class health extends Component {
+  render() {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         first
+      }
+    }
+    return (
+      <div>health</div>
+    )
+  }
+}
